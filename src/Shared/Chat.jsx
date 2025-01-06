d129251957bf317f45bcdf650ebbbbfa5bf0d0ed
ayon008 <shariar.ayon128@gmail.com>
2025-01-07 00:29:58 +0600

    addexd

--- src/Shared/Chat.jsx
+++ src/Shared/Chat.jsx
@@ -1,12 +1,39 @@
-'use client'
-import React from 'react';
-import { CustomChat, FacebookProvider } from 'react-facebook';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const Chat = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        // Ensure this code runs only on the client side
+        setIsClient(true);
+
+        // Dynamically load Facebook SDK
+        const script = document.createElement('script');
+        script.src = 'https://connect.facebook.net/en_US/sdk.js';
+        script.async = true;
+        script.defer = true;
+        script.crossOrigin = 'anonymous';
+        script.onload = () => {
+            if (window.FB) {
+                window.FB.init({
+                    appId: '1121645909753321',
+                    autoLogAppEvents: true,
+                    xfbml: true,
+                    version: 'v14.0', // Update to the latest Facebook API version
+                });
+            }
+        };
+        document.body.appendChild(script);
+    }, []);
+
+    if (!isClient) return null; // Prevent rendering on the server
+
     return (
         <div>
-            <h3>Ayon</h3>
-            <FacebookProvider appId="1121645909753321" chatSupport>
+            <FacebookProvider appId="1121645909753321">
                 <CustomChat pageId="498761066660090" minimized={true} />
             </FacebookProvider>
         </div>
