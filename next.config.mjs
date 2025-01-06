e02b2b64642c234994d83de3d106690266ceddda
ayon008 <shariar.ayon128@gmail.com>
2025-01-07 02:30:11 +0600

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: false,
+  images: {
+    domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
+  },
+};
+
+export default nextConfig;
