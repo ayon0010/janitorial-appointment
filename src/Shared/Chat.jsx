182247413826eeab03739a4842e5269f6a1c850f
ayon008 <shariar.ayon128@gmail.com>
2025-01-07 03:13:56 +0600

    xx

--- src/Shared/Chat.jsx
+++ src/Shared/Chat.jsx
@@ -1,13 +1,39 @@
 'use client';
-import React from 'react';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+import React, { useEffect } from 'react';
 
 const Chat = () => {
+    useEffect(() => {
+        // Inject the Facebook SDK
+        const script = document.createElement('script');
+        script.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+
+        // Initialize the Facebook SDK
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                xfbml: true,
+                version: 'v17.0', // Replace with the latest version
+            });
+        };
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
-        <MessengerCustomerChat
-            pageId="498761066660090"
-            appId="619890160420977"
-        />
+        <>
+            {/* Facebook Customer Chat Plugin Code */}
+            <div id="fb-root"></div>
+            <div
+                className="fb-customerchat"
+                attribution="biz_inbox"
+                page_id="498761066660090"
+                app_id="619890160420977"
+            ></div>
+        </>
     );
 };
 
