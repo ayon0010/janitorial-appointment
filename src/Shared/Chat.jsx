772de00fb3b2b7624221fe4a2be33dfc5536ae7d
ayon008 <shariar.ayon128@gmail.com>
2025-01-07 13:59:35 +0600

    xyry

--- src/Shared/Chat.jsx
+++ src/Shared/Chat.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 import { useEffect } from 'react';
 
@@ -43,48 +42,4 @@ export default MessengerChat;
 
 //  page_id=""
 //                 app_id=""
-=======
-"use client"; // Ensure this is a client component
-import { useEffect } from "react";
 
-const Chat = () => {
-  useEffect(() => {
-    // Ensure the code only runs on the client side
-    if (typeof window !== "undefined") {
-      window.fbAsyncInit = function () {
-        FB.init({
-          appId: "619890160420977", // Your Facebook App ID
-          xfbml: true,
-          version: "v17.0", // Use the latest SDK version
-        });
-      };
-
-      // Load the Facebook SDK script
-      (function (d, s, id) {
-        if (d.getElementById(id)) return;
-        const js = d.createElement(s);
-        js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-        const fjs = d.getElementsByTagName(s)[0];
-        fjs.parentNode.insertBefore(js, fjs);
-      })(document, "script", "facebook-jssdk");
-    }
-  }, []);
-
-  return (
-    <>
-      <div id="fb-root"></div>
-      <div
-        className="fb-customerchat"
-        attribution="setup_tool"
-        page_id="498761066660090" // Your Facebook Page ID
-        theme_color="#0084ff" // Customize the theme color
-        logged_in_greeting="Hi! How can we assist you?"
-        logged_out_greeting="Hi! Please log in to chat with us."
-      ></div>
-    </>
-  );
-};
-
-export default Chat;
->>>>>>> 7c548443f9a9f7fdb8b0a3b249c66a09e55e7a82
