373a54d7d81b22361527e7ec74e8d526f0c50f22
ayon008 <shariar.ayon128@gmail.com>
2025-02-01 00:29:04 +0600

    added

--- src/app/(main)/loading.js
+++ src/app/(main)/loading.js
@@ -1,10 +1,11 @@
-import Dot from "@/ui/Dot";
+// import Dot from "@/ui/Dot";
 
 const Loading = () => {
     return (
         <div className="flex items-center justify-center pt-40 pb-20 px-10">
             <div className="flex items-center justify-center">
-                <Dot width={'w-[400px]'} />
+                {/* <Dot width={'w-[400px]'} /> */}
+                <h1>Ayon</h1>
             </div>
         </div>
     );
