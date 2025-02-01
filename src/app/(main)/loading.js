07f8d793511ae6131e81a9f7d78eff9b443e9fd0
ayon008 <shariar.ayon128@gmail.com>
2025-02-01 15:35:20 +0600

    added

--- src/app/(main)/loading.js
+++ src/app/(main)/loading.js
@@ -1,17 +1,17 @@
-'use client'
-import dynamic from 'next/dynamic';
-const Dot = dynamic(() => import('@/ui/Dot'), {
-    ssr: false, // Optional if you don't want server-side rendering
-});
+// 'use client'
+// import dynamic from 'next/dynamic';
+// const Dot = dynamic(() => import('@/ui/Dot'), {
+//     ssr: false, // Optional if you don't want server-side rendering
+// });
 
-const Loading = () => {
-    return (
-        <div className="flex items-center justify-center pt-40 pb-20 px-10">
-            <div className="flex items-center justify-center">
-                <Dot width={'w-[400px]'} />
-            </div>
-        </div>
-    );
-};
+// const Loading = () => {
+//     return (
+//         <div className="flex items-center justify-center pt-40 pb-20 px-10">
+//             <div className="flex items-center justify-center">
+//                 <Dot width={'w-[400px]'} />
+//             </div>
+//         </div>
+//     );
+// };
 
-export default Loading;
+// export default Loading;
