35890e41b10983e14ddcc6300abe38ffac23e79c
ayon008 <shariar.ayon128@gmail.com>
2025-02-01 15:45:06 +0600

    added

--- src/app/(main)/loading.js
+++ src/app/(main)/loading.js
@@ -1,10 +1,11 @@
 'use client'
 import dynamic from 'next/dynamic';
-const Dot = dynamic(() => import('@/ui/Dot'), {
-    ssr: false, // Optional if you don't want server-side rendering
-});
+
+const Dot = dynamic(() => import('@/ui/Dot'), { ssr: false });
 
 const Loading = () => {
+    if (typeof window === 'undefined') return null; // Ensures only renders client-side
+
     return (
         <div className="flex items-center justify-center pt-40 pb-20 px-10">
             <div className="flex items-center justify-center">
