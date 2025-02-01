6b9390b51f8f2830d0db091b30dd108bcbf34489
ayon008 <shariar.ayon128@gmail.com>
2025-02-01 19:51:41 +0600

    added

--- src/app/(main)/loading.js
+++ src/app/(main)/loading.js
@@ -1,15 +1,15 @@
-'use client'
-import dynamic from 'next/dynamic';
+// 'use client'
+// import dynamic from 'next/dynamic';
 
-const Dot = dynamic(() => import('@/ui/Dot'), { ssr: false });
+// const Dot = dynamic(() => import('@/ui/Dot'), { ssr: false });
 
 const Loading = () => {
-    if (typeof window === 'undefined') return null; // Ensures only renders client-side
+    // if (typeof window === 'undefined') return null; // Ensures only renders client-side
 
     return (
         <div className="flex items-center justify-center pt-40 pb-20 px-10">
             <div className="flex items-center justify-center">
-                <Dot width={'w-[400px]'} />
+                {/* <Dot width={'w-[400px]'} /> */}
             </div>
         </div>
     );
