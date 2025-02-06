5fb7e06693aa5017448af223896775082726297a
ayon008 <shariar.ayon128@gmail.com>
2025-02-07 03:26:38 +0600

    addedda

--- src/app/(main)/loading.js
+++ src/app/(main)/loading.js
@@ -1,56 +1,44 @@
-// 'use client'
-// import { useEffect, useRef, useState } from 'react';
+'use client'
+import { useEffect, useRef, useState } from 'react';
 
-// const Loading = () => {
-//     const [isClient, setIsClient] = useState(false);
-//     const containerRef = useRef(null);
+const Loading = () => {
+    const [isClient, setIsClient] = useState(false);
+    const containerRef = useRef(null);
 
-//     // Check if the component is rendered on the client-side
-//     useEffect(() => {
-//         setIsClient(true);
-//     }, []);
+    // Check if the component is rendered on the client-side
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
-//     useEffect(() => {
-//         if (isClient) {
-//             const lottie = require('lottie-web'); // Import lottie only on the client-side
-//             const animation = require('@/../public/assets/Animation - 1722287102834.json');
+    useEffect(() => {
+        if (isClient) {
+            const lottie = require('lottie-web'); // Import lottie only on the client-side
+            const animation = require('@/../public/assets/Animation - 1722287102834.json');
 
-//             const animationInstance = lottie.loadAnimation({
-//                 container: containerRef.current,
-//                 renderer: 'svg',
-//                 loop: true,
-//                 autoplay: true,
-//                 animationData: animation,
-//             });
+            const animationInstance = lottie.loadAnimation({
+                container: containerRef.current,
+                renderer: 'svg',
+                loop: true,
+                autoplay: true,
+                animationData: animation,
+            });
 
-//             // Cleanup the animation on component unmount
-//             return () => animationInstance.destroy();
-//         }
-//     }, [isClient]);
+            // Cleanup the animation on component unmount
+            return () => animationInstance.destroy();
+        }
+    }, [isClient]);
 
-//     if (!isClient) {
-//         return null; // Render nothing on the server-side
-//     }
+    if (!isClient) {
+        return null; // Render nothing on the server-side
+    }
 
-//     return (
-//         <div className="flex items-center justify-center pt-40 pb-20 px-10">
-//             <div className="flex items-center justify-center">
-//                 <div ref={containerRef} className="h-auto w-[400px]" />
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Loading;
-
-import React from 'react';
-
-const loading = () => {
     return (
-        <div>
-            
+        <div className="flex items-center justify-center pt-40 pb-20 px-10">
+            <div className="flex items-center justify-center">
+                <div ref={containerRef} className="h-auto w-[400px]" />
+            </div>
         </div>
     );
 };
 
-export default loading;
+export default Loading;
