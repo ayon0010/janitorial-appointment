849e28b1afdd446013da37b344fdc9321a0a60d0
ayon008 <shariar.ayon128@gmail.com>
2025-02-08 15:51:06 +0600

    er

--- src/app/(main)/book-an-appointment/page.js
+++ src/app/(main)/book-an-appointment/page.js
@@ -1,10 +1,10 @@
-import AppointmentForm from '@/ui/AppointmentForm';
+// import AppointmentForm from '@/ui/AppointmentForm';
 import React from 'react';
 
 const page = () => {
     return (
         <div className='bg-white'>
-            <AppointmentForm />
+            {/* <AppointmentForm /> */}
         </div>
     );
 };
