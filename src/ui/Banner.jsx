02e644e0e102e473114e8b5413e0fd8f0ac0bfd3
ayon008 <shariar.ayon128@gmail.com>
2025-02-12 20:22:58 +0600

    ad

--- src/ui/Banner.jsx
+++ src/ui/Banner.jsx
@@ -31,21 +31,21 @@ const DynamicBanner = ({
     isTertiaryButton
 }) => {
     return (
-        <div className="flex flex-col relative min-h-[600px] max-h-[800px] h-auto">
+        <div className="flex flex-col relative min-h-[600px] max-h-[800px] h-auto w-full">
             {/* Background Images */}
             <div className="absolute inset-0 z-20 h-full">
                 <Image
                     src={desktopImage}
                     alt="Janitorial appointment and commercial cleaning leads to grow your business"
                     fill
-                    className="2xl:block xl:block hidden"
+                    className="2xl:block xl:block hidden w-full"
                     loading="lazy"
                 />
                 <Image
                     src={mobileImage}
                     alt="Mobile view showcasing janitorial appointments and commercial cleaning leads for business growth"
                     fill
-                    className="min-h-[400px] object-center 2xl:hidden xl:hidden block"
+                    className="min-h-[600px] object-center 2xl:hidden xl:hidden block w-full"
                     loading="lazy"
                 />
             </div>
@@ -193,6 +193,8 @@ const Banner = () => {
                 {/* Slider 5 */}
                 <SwiperSlide>
                     <div className="banner-6 flex flex-col h-full relative">
+                        {/* Overlays */}
+                        <div className="absolute inset-0 bg-black opacity-40 z-30"></div>
                         <div className='my-auto 2xl:pl-40 xl:pl-40 2xl:w-1/2 xl:w-1/2 w-full pl-10 2xl:pr-0 xl:pr-0 pr-10 z-40'>
                             <h2 className='2xl:text-6xl xl:text-5xl text-3xl font-semibold 2xl:banner-text xl:banner-text text-white'>
                                 Elevate Your Brand, Expert <span className='text-green-600'>Digital Marketing, Web & App Solutions</span>
