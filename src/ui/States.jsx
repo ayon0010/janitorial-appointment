e7a46a6bfca613943f37060d3f6880f7db571c23
ayon008 <shariar.ayon128@gmail.com>
2025-02-27 15:30:05 +0600

    ad

--- src/ui/States.jsx
+++ src/ui/States.jsx
@@ -3,6 +3,8 @@ import LeadsByCity from "@/ui/LeadsByCity";
 import { AppointmentButton } from "./Lead";
 import { gilroy } from "@/app/(main)/layout";
 import image from '../../public/assets/exclusive-commercial-cleaning-leads-1.webp'
+import SectionTitles from "./SectionTitles";
+import ContactInfo from "./ContactInfo";
 
 function capitalizeFirstLetter(string) {
     if (!string) return '';
@@ -22,10 +24,11 @@ const States = async ({ States }) => {
                 <h1 className={`${gilroy.className}`}>Home  {'>'}  {capitalizeFirstLetter(leads)}  {'>'}  {capitalizeFirstLetter(states)}</h1>
             </div>
             <div className="py-20 px-6">
+                {/* <SectionTitles heading={'Choose Cities'} /> */}
                 <div>
                     <div className="flex 2xl:flex-row xl:flex-row flex-col items-center gap-4 justify-center">
-                        <h4 className="text-xl inter font-medium text-center">
-                            {capitalizeFirstLetter(leads)} in {decodeState}
+                        <h4 className={`text-2xl inter font-medium text-center ${gilroy.className}`}>
+                            Available {capitalizeFirstLetter(leads)} in {decodeState}
                         </h4>
                         <p className="text-[#6941C6] inter text-sm font-medium py-[3px] px-[10px] bg-[#F9F5FF] rounded-[18px]">
                             {data?.length} leads
@@ -36,6 +39,16 @@ const States = async ({ States }) => {
                     </p>
                 </div>
                 <LeadsByCity data={data} states={states} />
+                <div className="2xl:w-3/4 xl:w-3/4 w-full mx-auto my-20">
+                    <SectionTitles heading={`Get Exclusive Commercial Cleaning leads in ${decodeState}`} />
+                    <p className={`text-center ${gilroy.className} my-10`}>
+                        Located at the heart of the industry, we specialize in providing commercial cleaning leads in {decodeState} to help businesses grow. Our janitorial appointment-setting service connects you with high-quality clients actively seeking cleaning services across {decodeState}. We generate exclusive commercial cleaning leads by targeting decision-makers who need reliable cleaning solutions. Whether you're a startup or an established cleaning company, our dedicated team works around the clock to deliver qualified janitorial appointments in {decodeState}, helping you maximize your business potential and keep your pipeline full of prospects.
+                    </p>
+                </div>
+            </div>
+            <div className="mb-10">
+                <SectionTitles heading={'Contact Us'} />
+                <ContactInfo />
             </div>
         </>
     );
