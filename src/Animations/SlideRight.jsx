28becc0d1816bb5e01f13cfe93a00c760cb558bf
ayon008 <shariar.ayon128@gmail.com>
2025-05-18 22:00:01 +0600

    a

--- src/Animations/SlideRight.jsx
+++ src/Animations/SlideRight.jsx
@@ -2,7 +2,7 @@
 import { motion, useInView } from "motion/react";
 import { useRef } from "react";
 
-const SlideRight = ({ children, delay = 0, amount = 0.1 }) => {
+const SlideRight = ({ children, delay = 0, amount = 0.5 }) => {
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true, amount });
 
@@ -11,11 +11,7 @@ const SlideRight = ({ children, delay = 0, amount = 0.1 }) => {
             ref={ref}
             initial={{ opacity: 0, x: 200 }} // Less distance for better performance on small devices
             whileInView={{ opacity: 1, x: 0 }}
-            transition={{
-                delay,
-                duration: 0.5,
-                ease: [0.25, 0.46, 0.45, 0.94], // easeOutCubic
-            }}
+            transition={{ type: "spring", bounce: 0.25 }}
             viewport={{ once: true }}
             className="transform-gpu"
         >
