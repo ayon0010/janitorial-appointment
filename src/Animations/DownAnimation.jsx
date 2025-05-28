784049f4d9355d37e5a2805d2002958d7339ef80
ayon008 <shariar.ayon128@gmail.com>
2025-05-28 11:19:59 +0600

    a

--- src/Animations/DownAnimation.jsx
+++ src/Animations/DownAnimation.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { motion } from "motion/react"
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 
 const DownAnimation = ({ animationKey, children, delay = 0 }) => {
     const pathName = usePathname();
